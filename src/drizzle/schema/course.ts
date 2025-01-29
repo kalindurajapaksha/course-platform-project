1dfd0d8cec5304129fc3c6c67a6b573d154b56d5
Kalindu Rajapaksha <kalindurajapaksha@gmail.com>
2025-01-29 23:39:12 +0530

    drizzle and clerk setup

---
+++ src/drizzle/schema/course.ts
@@ -0,0 +1,20 @@
+import { relations } from "drizzle-orm";
+import { pgTable, text } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "../schemaHelpers";
+import { CourseProductTable } from "./courseProduct";
+import { CourseSectionTable } from "./courseSection";
+import { UserCourseAccessTable } from "./userCourseAccess";
+
+export const CourseTable = pgTable("courses", {
+  id,
+  name: text().notNull(),
+  description: text().notNull(),
+  createdAt,
+  updatedAt,
+});
+
+export const CourseRelationships = relations(CourseTable, ({ many }) => ({
+  courseProducts: many(CourseProductTable),
+  courseSections: many(CourseSectionTable),
+  userCourseAccesses: many(UserCourseAccessTable),
+}));
